46dbfc7d53ee10f047c8c396b992fd9816c30dbd
nervinjf <nervinjflores@gmail.com>
2022-09-02 14:23:21 -0400

    nuevos cambio

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import axios from 'axios';
-import useFetch from './useFetch';
-import Weather from './Weather';
+import useFetch from './Hooks/useFetch';
+import Weather from './Component/Weather';
 
 function App() {
 
--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react'
 import axios from 'react'
-import useFetch from './useFetch';
+import useFetch from '../Hooks/useFetch';
 
 const Weather = () => {
 
