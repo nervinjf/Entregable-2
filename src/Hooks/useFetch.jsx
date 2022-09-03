2d7066d85dc66793974267217243c757ce1980f7
nervinjf <nervinjflores@gmail.com>
2022-09-02 21:51:12 -0400

    cambios

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useState, useEffect } from 'React';
+
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const useFetch = (accessApi) => {
